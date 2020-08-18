03e719ffd8e328195fdd0f188699cb86b78fb9a9
jurge <jurgen1010@hotmail.com>
2020-08-17 21:58:18 -0500

    Se crea el maestro detalle para agreagar departamentos a un pais

--- OnSale.Web/Controllers/CountriesController.cs
+++ OnSale.Web/Controllers/CountriesController.cs
@@ -3,7 +3,6 @@ using Microsoft.EntityFrameworkCore;
 using OnSale.Common.Entities;
 using OnSale.Web.Data;
 using System;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace OnSale.Web.Controllers
@@ -20,10 +19,12 @@ namespace OnSale.Web.Controllers
         // GET: Countries
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Countries.ToListAsync());
+            return View(await _context.Countries
+                .Include(c => c.Departments)// Ademas de los paises, incluyame los departamentos
+                .ToListAsync());
         }
 
-        // GET: Countries/Details/5
+        // GET: Countries/Details/
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -32,6 +33,8 @@ namespace OnSale.Web.Controllers
             }
 
             var country = await _context.Countries
+                .Include(c => c.Departments)//Incluyame el pais pero dentro de el sus departamentos relacionados, equivalente a implementar un INNER JOIN
+                .ThenInclude(d => d.Cities)//Asi mismo traigame las ciudades relacionadas al departamento, equivalente a otro INNER JOIN
                 .FirstOrDefaultAsync(m => m.Id == id);
             if (country == null)
             {
@@ -106,7 +109,7 @@ namespace OnSale.Web.Controllers
             {
                 return NotFound();
             }
-            
+
             if (ModelState.IsValid)
             {
                 try
@@ -154,9 +157,65 @@ namespace OnSale.Web.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-        private bool CountryExists(int id)
+        public async Task<IActionResult> AddDepartment(int? id)
         {
-            return _context.Countries.Any(e => e.Id == id);
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Country country = await _context.Countries.FindAsync(id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            Department model = new Department { IdCountry = country.Id };//para cuando se agrefue un departamento saber a que pais esta relacionado
+            return View(model);
         }
+
+        [HttpPost]//El Post recibe lo que se envia 
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddDepartment(Department department)
+        {
+            if (ModelState.IsValid)//Validamos que el modelo sea valido(que no sea duplicado y que no tenga mas de 50 caracteres)
+            {
+                Country country = await _context.Countries//Busque en la base de datos el country
+                    .Include(c => c.Departments)
+                    .FirstOrDefaultAsync(c => c.Id == department.IdCountry);
+                if (country == null)
+                {
+                    return NotFound();
+                }
+
+                try
+                {
+                    department.Id = 0;//Le indico que es el 0 para que sepa que estoy agregando un Id mas no actualizando uno
+                    country.Departments.Add(department);//Al coutry en la coleccion Departments agregue el department
+                    _context.Update(country);//Finalmente hacemos un update al COuntry
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction($"{nameof(Details)}/{country.Id}");//Redireccionaos al detalle del pais al que se agrego un departamento.
+
+                }
+                catch (DbUpdateException dbUpdateException)
+                {
+                    if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    {
+                        ModelState.AddModelError(string.Empty, "There are a record with the same name.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    ModelState.AddModelError(string.Empty, exception.Message);
+                }
+            }
+
+            return View(department);
+        }
+
     }
 }
